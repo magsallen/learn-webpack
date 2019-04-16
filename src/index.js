9876b0f4ecbe04668119d00e50a2b6429eb5544c
Maggie Allen <maggie.allen@ft.com>
2019-04-16 15:45:42 +0100

    add mini-css-extract-plugin and output css as a separate bundle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Header } from './components/header'
-import { Content } from './components/content'
-import { Footer } from './components/footer'
+import { Header } from '../components/header'
+import { Content } from '../components/content'
+import { Footer } from '../components/footer'
 
 import { OnReady } from './utils'
-import * as browser from './browser'
+import * as browser from '../browser'
 
-import './styles/shared';
+// import '../styles/shared';
+// import '../styles/header-footer';
+// import '../styles/body';
 
 ReactDOM.render(
 	<OnReady callback={() => browser.init()}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,26 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './start.js',
+  entry: {
+		main: './src/index.js',
+		styles: './styles/shared.scss'
+	},
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'start.bundle.js'
+    filename: '[name].bundle.js'
 	},
 	resolve: { extensions: [ '.js', '.jsx', '.scss' ] }, // enables bare imports (without the .ext)
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		})
+	],
 	module: {
 		rules: [
 			{ test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }, // babel webpack plugin - transpile js files with babel
-			{ test: /\.(css|scss)$/, exclude: /node_modules/, use: ['style-loader', 'css-loader', 'sass-loader'] } // babel webpack plugin - transpile scss
+			{ test: /\.(css|scss)$/, exclude: /node_modules/, use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader'] } // babel webpack plugins - transpile scss to css
 		]
-	},
-	devtool: 'eval-source-map'
+	}
 };
